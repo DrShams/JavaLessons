c9ce2bd2082cc466f12c7f24c094a35aa6550c4f
DrShams <sithis02@mail.ru>
2022-07-28 18:05:29 +0500

    added WordCount

---
+++ Homework_2/src/Math/Division/Division.java
@@ -0,0 +1,15 @@
+package Math.Division;
+
+public class Division {
+
+    public Division() {
+
+    }
+
+    public Division(double a, double b) {
+    }
+
+    public static double divs(double a, double b){
+        return a/b;
+    }
+}
